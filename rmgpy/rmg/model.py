31b784e4fd0de2fa8aee1ac6bcb758953645e3aa
MarkGoldman <goldmanm@mit.edu>
2017-06-13 07:43:26 -0400

    change default kinetics estimator to rate rules
    group additivity currently doesn't work in RMG (and more people
    use rate rules currently), so the default estimator is changed
    to rate rules.

--- rmgpy/rmg/model.py
+++ rmgpy/rmg/model.py
@@ -227,7 +227,7 @@ class CoreEdgeReactionModel:
         self.pressureDependence = None
         self.quantumMechanics = None
         self.verboseComments = False
-        self.kineticsEstimator = 'group additivity'
+        self.kineticsEstimator = 'rate rules'
         self.indexSpeciesDict = {}
         self.saveEdgeSpecies = False
 
