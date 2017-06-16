0a9990b6e4f1149c4c8ba683c9496221145ee190
Mark Goldman <goldmanm@mit.edu>
2017-06-16 15:10:36 -0400

    squash! refactored Reaction.degeneracy property

--- rmgpy/reaction.py
+++ rmgpy/reaction.py
@@ -170,7 +170,9 @@ class Reaction:
             else:
                 degeneracyRatio = (new*1.0) / self._degeneracy
             self.kinetics.changeRate(degeneracyRatio)
-            logging.debug('did not modify A factor when modifying degeneracy since the reaction rate was not set')
+        else:
+            logging.debug('did not modify A factor when modifying degeneracy since' \
+                          'the reaction rate was not set')
         # set new degeneracy
         self._degeneracy = new
     degeneracy = property(__getDegneneracy, __setDegeneracy)
